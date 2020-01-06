09c5583a8330ced0a8f3bd0e7f0ebf89fe9f24dc
unknown <sarmad99@msn.com>
2020-01-06 18:18:53 -0500

    more jscript array stuf

--- Node JS tests/app.js
+++ Node JS tests/app.js
@@ -10,13 +10,20 @@ but we will use shortHAND
 to run this install NODE JS first , Open Terminal and RUN node filename.js
 */
 
-const age = 18;
-
+const age = 72;
+// simple conditional if else
 age < 15 ? console.log("You are underage") : console.log("You are a READY");
+
+// multiple conditional if elses inside eachother
+age > 50 //if age > 50 enter the >70 condition. Else go straight to last statement ( you are bellow 30)
+  ? age > 70
+    ? console.log("You are getting really old above 70 ") // only if >70
+    : console.log("You are Not so so old yet between 50-70") //this if >50 but <70
+  : console.log("You are bellow 50"); //this line only works if we are < 50
 
 // you want to take input from user create an HTML file. remember to link it to JS file
 function getUserName() {
-  var nameField = document.getElementById("nameField").value;
+  var nameField = document.getElementById("nameField").value; //notice we want value
   var result = document.getElementById("result");
 
   if (nameField.length < 3) {
@@ -27,3 +34,23 @@ function getUserName() {
     alert(nameField);
   }
 }
+
+// how to convert something to String ? you add + "", use typeof
+const number1 = 20;
+console.log(number1);
+console.log(typeof number1);
+number2 = number1 + ""; // we have a string now
+console.log(typeof number2);
+
+// filling arrays
+const users = Array(5);
+console.log("this is users array No fill", users);
+var users2 = Array(5).fill("Something");
+console.log("this is users 2 array with fill", users2);
+
+//more array stuff
+const users3 = ["Ed", "john", "Unemployed", "Ok", "Ok", "Ok", "Why Not"];
+console.log(users3);
+//we want to show unique items no duplicates ? use NEW SET function, it only creates unique values no dups
+const unique = Array.from(new Set(users3));
+console.log("Shows no duplicates", unique);
